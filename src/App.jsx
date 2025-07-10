6820ec6acb94f53746e9a135bd22e3382f787a01
nazaninJh <nazanin.jh97@gmail.com>
2025-07-10 17:56:27 +0330

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-3xl text-indigo-300 flex-center h-screen">App</div>
+  );
 };
 
 export default App;
